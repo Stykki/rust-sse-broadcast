ecb679f3bdf3cd01f9190e71078210b7ba4f632c
Stykki <vignirv19@ru.is>
2023-03-05 13:00:54 +0000

    Initial commit

---
+++ src/broadcast.rs
@@ -0,0 +1,90 @@
+use std::{collections::HashMap, sync::Arc, time::Duration};
+
+use actix_web_lab::sse::{self, ChannelStream, Sse};
+use parking_lot::Mutex;
+
+use tokio::time::interval;
+
+#[derive(Debug, Clone, Default)]
+struct BroadcasterInner {
+    clients: HashMap<String, Vec<sse::Sender>>,
+}
+
+pub struct BroadcastStore {
+    inner: Mutex<BroadcasterInner>,
+}
+
+impl BroadcastStore {
+    pub fn new() -> Arc<Self> {
+        let this = Arc::new(BroadcastStore {
+            inner: Mutex::new(BroadcasterInner::default()),
+        });
+
+        BroadcastStore::spawn_ping(Arc::clone(&this));
+
+        this
+    }
+
+    fn spawn_ping(this: Arc<Self>) {
+        actix_web::rt::spawn(async move {
+            let mut interval = interval(Duration::from_secs(10));
+
+            loop {
+                interval.tick().await;
+                this.remove_stale_clients().await;
+            }
+        });
+    }
+
+    async fn remove_stale_clients(&self) {
+        let clients = self.inner.lock().clients.clone();
+
+        let mut ok_clients = HashMap::new();
+
+        // iterate over all vectors in the hashmap and remove the stale clients
+        for (channel, senders) in clients {
+            let mut ok_senders = Vec::new();
+            for sender in senders {
+                if sender
+                    .send(sse::Event::Comment("ping".into()))
+                    .await
+                    .is_ok()
+                {
+                    ok_senders.push(sender.clone());
+                }
+            }
+            ok_clients.insert(channel, ok_senders);
+        }
+
+        self.inner.lock().clients = ok_clients;
+    }
+
+    pub async fn add_client(&self, id: String) -> Sse<ChannelStream> {
+        let (tx, rx) = sse::channel(10);
+        tx.send(sse::Data::new("connected")).await.unwrap();
+
+        // add the client to the list of clients
+        let mut inner = self.inner.lock();
+        let x = inner.clients.entry(id.clone()).or_insert_with(Vec::new);
+        x.push(tx);
+
+        log::info!("added client to channel {}", id);
+
+        rx
+    }
+
+    pub async fn broadcast(&self, channel: &str, msg: &str) {
+        let inner = self.inner.lock();
+        let recv = inner.clients.get(channel);
+
+        match recv {
+            Some(_) => {
+                log::info!("broadcasting message to channel {}", channel);
+                for sender in inner.clients.get(channel).unwrap() {
+                    sender.send(sse::Data::new(msg)).await.unwrap();
+                }
+            }
+            None => log::info!("channel {} not found", channel),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::{io, sync::Arc};
+
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web_lab::{extract::Path, respond::Html};
+use serde::{Deserialize, Serialize};
+
+use crate::broadcast::BroadcastStore;
+
+mod broadcast;
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let broadcast_store = broadcast::BroadcastStore::new();
+
+    log::info!("starting HTTP server at http://localhost:8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::from(Arc::clone(&broadcast_store)))
+            .service(index)
+            .service(event_stream)
+            .service(broadcast_msg)
+            .service(bc)
+            .wrap(Logger::default())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .workers(2)
+    .run()
+    .await
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    Html(include_str!("index.html").to_string())
+}
+
+#[get("/events/{channel}")]
+async fn event_stream(
+    broadcast_store: web::Data<BroadcastStore>,
+    Path((channel,)): Path<(String,)>,
+) -> impl Responder {
+    broadcast_store.add_client(channel).await
+}
+
+#[post("/broadcast/{channel}/{msg}")]
+async fn broadcast_msg(
+    broadcast_store: web::Data<BroadcastStore>,
+    Path((channel, msg)): Path<(String, String)>,
+) -> impl Responder {
+    broadcast_store.broadcast(&channel, &msg).await;
+    HttpResponse::Ok().body("msg sent")
+}
+
+#[derive(Serialize, Deserialize)]
+struct BcPayload {
+    channel: String,
+    msg: String,
+}
+
+#[post("/broadcast/")]
+async fn bc(
+    bc_payload: web::Json<BcPayload>,
+    broadcast_store: web::Data<BroadcastStore>,
+) -> impl Responder {
+    broadcast_store
+        .broadcast(&bc_payload.channel, &bc_payload.msg)
+        .await;
+
+    return HttpResponse::Ok().body("msg sent");
+}
